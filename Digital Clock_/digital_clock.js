e87109447fb6cc8efb6f696c1ef04476d0d9cd88
Varshini0817 <113962586+Varshini0817@users.noreply.github.com>
2025-06-15 22:20:16 +0530

    Digital_clock

---
+++ Digital Clock_/digital_clock.js
@@ -0,0 +1,26 @@
+function updateClock(){
+    let time = new Date();
+    let hours = time.getHours();
+    let minutes = time.getMinutes();
+    let seconds = time.getSeconds();
+
+    am_pm = "AM";
+
+    if (hours > 12){
+        am_pm = "PM"
+    }
+    else if (hours == 0){
+        hours = 12;
+    }
+
+    hours = hours < 10 ? "0"+hours : hours
+    minutes = minutes < 10 ? "0"+ minutes : minutes
+    seconds = seconds < 10 ? "0"+ seconds : seconds
+
+
+    let currTime = hours + ":" + minutes + ":" + seconds + am_pm;
+    document.getElementById("digital_clock").textContent = currTime;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
